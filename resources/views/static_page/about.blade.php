f74e367eea5f35a49121c80f899dae7b6fbe4f33
lxy <810967578@qq.com>
2021-02-23 06:16:15 +0000

    静态页面

---
+++ resources/views/static_page/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','guangyu')
+@section('content')
+<h1>关于</h1>
+<h2>关于</h2>
+@stop
---
+++ resources/views/static_page/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('content')
+<h1>首页</h1>
+<h2>首页</h2>
+@stop
